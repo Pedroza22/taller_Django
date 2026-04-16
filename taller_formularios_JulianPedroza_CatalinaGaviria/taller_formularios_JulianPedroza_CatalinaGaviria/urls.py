58c2b6dbc275742add4cca4e7ec90f847d170783
pedroza22 <julianpedrozaospina@gmail.com>
2026-04-16 09:03:18 -0500

    Configurar urls de solicitudes y asistencia

--- taller_formularios_JulianPedroza_CatalinaGaviria/taller_formularios_JulianPedroza_CatalinaGaviria/urls.py
+++ taller_formularios_JulianPedroza_CatalinaGaviria/taller_formularios_JulianPedroza_CatalinaGaviria/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('solicitudes/', include('solicitudes.urls')),
+    path('solicitudes/', include('solicitudes.urls', namespace='solicitudes')),
+    path('asistencia/', include('asistencia.urls', namespace='asistencia')),
+    path('', RedirectView.as_view(url='/solicitudes/', permanent=True)),
 ]
 
 if settings.DEBUG:
