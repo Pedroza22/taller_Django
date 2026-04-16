07df231e606aa17083c0aa7826dc8f5d60b95c42
johanagaviria19 <johana.gaviria@campusucc.edu.co>
2026-04-16 08:50:45 -0500

    Fix forms y modelos de asistencia y solicitudes para cumplir requerimientos

--- taller_formularios_JulianPedroza_CatalinaGaviria/solicitudes/forms.py
+++ taller_formularios_JulianPedroza_CatalinaGaviria/solicitudes/forms.py
@@ -5,3 +5,6 @@ class SolicitudForm(forms.ModelForm):
     class Meta:
         model = Solicitud
         fields = '__all__'
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date'}),
+        }
